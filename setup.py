8c690b84e10e49c1fea941ab6229c6bd88662a34
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2017-07-28 08:17:46 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -3,20 +3,35 @@
 import os
 import sys
 
-assert "ASCDS_INSTALL" in os.environ
+assert "ASCDS_INSTALL" in os.environ, "Please run this after CIAO has been setup"
+
+#
+# I need the pythonX.Y version for the purelib's.  Can't find a way 
+# to pass in so that it gets picked up by setup.cfg so we just set
+# and env variable and that works.
+#
 
 ver = sys.version_info
 os.environ["PYVER"] = "python{}.{}".format(ver[0],ver[1])
 
+###
+### distutils replaces the shebang /usr/bin/env python with sys.executable.
+### This doesn't work with CIAO where we need to run the wrapper  in 
+### $ASCDS_INSTALL/bin instead of $ASCDS_INSTALL/ots/bin
+###
+### So, we change it before running setup()
+
+sys.executable = os.path.join( os.environ["ASCDS_INSTALL"], "bin", "python"  )
+
+
+
 from distutils.core import setup
-
-
 setup( name='qocat',
         version='0.0.1',
         description='Query the Chandra Observation Catalog by ObsId',
         author='Anonymous',
         author_email='WhoDat@cfa.harvard.edu',
         url='https://github.com/kglotfelty/ocat/',
-        scripts = ["qocat"],
+        scripts = ["qocat"]
         )
 
